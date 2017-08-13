33b6dbc03de5bb87df7b69ea4d795f1df3f8d9a2
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-08-12 23:50:10 -0500

    Crea la ruta POST

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get '/products', to: 'products#index'
-  get '/products/new', to: 'products#new'
-  
+  get '/products/new', to: 'products#new', as: 'new_product'
+
+  post '/products', to: 'products#create'
+
 end
