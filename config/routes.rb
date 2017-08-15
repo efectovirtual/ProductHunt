e38bae98827f76d681bb00dcaf3c5556a07ceb71
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-08-14 22:38:50 -0500

    Agregar boton de edición de producto

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/products', to: 'products#create'
 
   get '/products/:id', to: 'products#show', as: 'product'
-  get '/products/:id/edit', to: 'products#edit'
+  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   
   patch '/products/:id', to: 'products#update'
 
