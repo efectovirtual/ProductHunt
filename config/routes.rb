ecc0b85c70bfec1a4432c066e2d0590d11fb65e6
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-08-15 17:12:31 -0500

    Autenticación Funcionabilidad

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new, :create]
+  root 'products#index'
 
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+
+  resources :users, only: [:new, :create]
   resources :products
 
-  root 'products#index' #muestra vista inicial
+  
 
 
 end
