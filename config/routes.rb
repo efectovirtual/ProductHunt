e85699bdf207a8249686842f2d167e978da96083
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-08-15 17:17:57 -0500

    Funcionabilidad de Salir

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
 
   resources :users, only: [:new, :create]
   resources :products
