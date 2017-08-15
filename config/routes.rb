6f408d6498ed9a6c762b5a89f059335e95de0d6a
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-08-14 22:31:55 -0500

    Crea la ruta PATCH update

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   get '/products/:id', to: 'products#show', as: 'product'
   get '/products/:id/edit', to: 'products#edit'
+  
+  patch '/products/:id', to: 'products#update'
 
 end
